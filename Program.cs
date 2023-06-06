b3ff25f712020a0f3e346f230e8d221dee274a99
ben <{username}@users.noreply.github.com>
2023-06-06 13:45:27 -0500

    create project and gitignore

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pomodoro_cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pomodoro-cli");
+        }
+    }
+}
