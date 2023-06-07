25183eab6a722df6820e6bce59ec136e4536a4c4
ben <{username}@users.noreply.github.com>
2023-06-07 17:20:13 -0500

    Add dependency injection, TestService, and configured logging. Created appsettings.Development.json, which currently just holds logging config.

---
+++ EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using pomodoro_cli.Services;
+
+namespace pomodoro_cli
+{
+    public class EntryPoint
+    {
+        private readonly ITestService testService;
+
+        public EntryPoint(ITestService testService)
+        {
+            this.testService = testService;
+        }
+
+        public void Execute()
+        {
+            this.testService.Test();
+        }
+    }
+}
